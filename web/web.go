b93cf0e6432bd3deb9974504ed6beaa7094647c8
Viktor Posloncec <viktor.posloncec@gmail.com>
2024-01-28 12:35:50 +0100

    Simplyfy web serving csv

--- web/web.go
+++ web/web.go
@@ -1,31 +1,28 @@
 package web
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/vposloncec/go-ssip/base"
 	"github.com/vposloncec/go-ssip/export"
-	"go.uber.org/zap"
 	"net/http"
+	"time"
 )
 
 func Serve(graph *base.Graph) {
 	http.HandleFunc("/nodes", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/csv")
-		w.Header().Set("Content-Disposition", "attachment; filename=nodes.csv")
-		_, err := export.NodesToCSV(graph.Nodes).WriteTo(w)
-		if err != nil {
-			return
-		}
+		//w.Header().Set("Content-Type", "text/csv")
+		//w.Header().Set("Content-Disposition", "attachment; filename=nodes.csv")
+		b := export.NodesToCSV(graph.Nodes)
+		reader := bytes.NewReader(b.Bytes())
+		http.ServeContent(w, r, "nodes", time.Now(), reader)
 	})
 	http.HandleFunc("/edges", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/csv")
-		w.Header().Set("Content-Disposition", "attachment; filename=edges.csv")
+		//w.Header().Set("Content-Type", "text/csv")
+		//w.Header().Set("Content-Disposition", "attachment; filename=edges.csv")
 		b := export.EdgesToCSV(graph.Connections)
-		var err error
-		_, err = b.WriteTo(w)
-		if err != nil {
-			zap.Error(err)
-		}
+		reader := bytes.NewReader(b.Bytes())
+		http.ServeContent(w, r, "edges", time.Now(), reader)
 	})
 
 	fmt.Println("Server is running on http://localhost:8080")
